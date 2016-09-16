b8cf3c0a3cc17cf198ab9513b83756892cc47795
Julian Biebelberg <jbiebelberg@gmail.com>
2016-09-16 14:16:02 -0400

    set up server js dependencies

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const cheerio = require('cheerio');
+const request = require('request');
+
+//Mongoose stuff
+mongoose.connect('mongodb://heroku_sl5t668c:d4gmc54o0iiei8jfqc27e2gife@ds033066.mlab.com:33066/heroku_sl5t668c');
+const db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function () {
+  console.log('Mongoose connection successful.');
+});
+
+//Public
+app.use(express.static('public'));
+
+//Body Parser
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+//Handlebars
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+
+app.listen(port);
